2493e70fa7ace13066f26b23e887e133f79c2230
Sosow20 <sofiacherifi1995@gmail.com>
2025-09-30 16:19:29 +0200

    update : fetch dataVolunteers and display to front

--- adaction_Back-end/server.js
+++ adaction_Back-end/server.js
@@ -5,6 +5,8 @@ import { Pool } from "pg"
 
 
 const app = express();
+app.use(express.json())
+app.use(cors({ origin: "http://127.0.0.1:5500" }));
 
 dotenv.config();
 
@@ -13,16 +15,31 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false }, // recommandé pour Neon afin de sécuriser la connexion
 });
 
-app.use(cors({ origin: "http://127.0.0.1:5500"}));
 
-app.get("/volunteers", async (req, res) => {  
+
+app.get("/volunteers", async (req, res) => {
   try {
-    
-  const result = await pool.query('SELECT * from volunteers')
-  res.json(result.rows)
+
+    const result = await pool.query('SELECT * from volunteers')
+    res.json(result.rows)
   } catch (error) {
     console.log(error)
   }
 });
 
-app.listen(3000, () => {  console.log("Serveur lancé sur http://localhost:3000");});
+app.get("/quantities", async(req, res) => {
+  try {
+    const quantity = await pool.query ('SELECT volunteers.name, volunteers.city,\
+      SUM(quantities.quantity) AS total_quantity\
+      FROM quantities\
+      INNER JOIN collections ON quantities.collection_id = collections.id\
+      INNER JOIN volunteers ON collections.volunteer_id = volunteers.id\
+      GROUP BY volunteers.id, volunteers.name')
+
+    res.json(quantity.rows)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.listen(3000, () => { console.log("Serveur lancé sur http://localhost:3000"); });
--- adaction_Front-end/js/volunteers.js
+++ adaction_Front-end/js/volunteers.js
@@ -1,11 +1,70 @@
+let paramString = window.location.search;
+let searchParams = new URLSearchParams(paramString);
+
+const city = searchParams.get('city-list')
+const name = searchParams.get('name-input')
+
 const API_URL = 'http://localhost:3000'
 
 
-async function fetchVolunteers() {
-    const response = await fetch(`${API_URL}/volunteers`)
-    const data = await response.json()
-    console.log(data)
-    
+// async function fetchVolunteers() {
+//     try {
+//         const response = await fetch(`${API_URL}/volunteers`)
+//         const data = await response.json()
+//         // console.log(data)
+//         return data
+
+//     } catch (error) {
+//         console.log(error)
+//     }
+
+// }
+
+async function fetchQuantities() {
+    try {
+        const response = await fetch(`${API_URL}/quantities`)
+        const data = await response.json()
+        console.log(data)
+        return data
+
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-fetchVolunteers()
+const profils = document.querySelectorAll('.volunteer-profil')
+
+const displayVolunteers = async () => {
+    try {
+        const dataVolunteers = await fetchQuantities();
+
+        profils.forEach((item, index) => {
+            const volunteer = dataVolunteers[index];
+
+            if (!volunteer) return;
+
+            item.innerHTML = `
+        <div>
+        <ul> 
+        <li> Nom: ${volunteer.name}</li>
+        <li> Localisation: ${volunteer.city}</li>
+        <li class='collect'> Nombre de déchets collectés : ${volunteer.total_quantity} </li>
+        </ul>
+        </div>
+        <button id="edit-btn"> modiifier </button>
+        <button id="delete-btn"> supprimer </button>
+        
+        `
+        })
+
+    } catch (error) {
+        console.log(error)
+
+    }
+
+
+}
+
+displayVolunteers()
+
+
