207cd6387032fe163cc718885ccbf0ed0f3ae503
Xinzhu99 <jxinzhu@gmail.com>
2025-10-02 18:05:12 +0200

    update: page add_collect feature add_collection is done

--- adaction_Back-end/server.js
+++ adaction_Back-end/server.js
@@ -1,8 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { Pool } from "pg"
-
+import { Pool } from "pg";
 
 const app = express();
 
@@ -14,70 +13,90 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false }, // recommandé pour Neon afin de sécuriser la connexion
 });
 
-app.use(cors({ origin: "http://127.0.0.1:5500"}));
+app.use(cors({ origin: "http://127.0.0.1:5500" }));
 
-app.get("/volunteers", async (req, res) => {  
+app.get("/volunteers", async (req, res) => {
   try {
-    
-  const result = await pool.query('SELECT * from volunteers')
-  res.json(result.rows)
+    const result = await pool.query("SELECT * from volunteers");
+    res.json(result.rows);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 });
 
-
 // 🚀 nouvelle route pour la page add_collect: ajouter une nouvelle collecte
 app.post("/add_collection", async (req, res) => {
-console.log("[POST/collection] body reçu:", req.body);
-const { volunteers_name, collections_date, collections_location, quantities} = req.body; //!req.body est stocké dans une variable d'objet
-try {
-//récupérer le id du volunteer
-	const volunteerResult = await pool.query(
-	"SELECT id from volunteers WHERE name = $1",[volunteers_name]);
-  console.log("volunteerResult", volunteerResult.rows);
+  console.log("[POST/collection] body reçu:", req.body);
+  const {
+    volunteers_name,
+    collections_date,
+    collections_location,
+    quantities,
+  } = req.body; //!req.body est stocké dans une variable d'objet
+  try {
+    //récupérer le id du volunteer
+    const volunteerResult = await pool.query(
+      "SELECT id from volunteers WHERE name = $1",
+      [volunteers_name]
+    );
+    console.log("volunteerResult", volunteerResult.rows);
 
-  if (volunteerResult.rows.length ===0){
-    return res.status(404).json({ok: false, message:"Bénévole non trouvé."})
-  };
+    if (volunteerResult.rows.length === 0) {
+      return res
+        .status(404)
+        .json({ ok: false, message: "Bénévole non trouvé." });
+    }
 
-  const volunteer_id = volunteerResult.rows[0].id;
+    const volunteer_id = volunteerResult.rows[0].id;
 
-// insérer la collecte dans collections
-  const insertCollection = await pool.query(
-	"INSERT INTO collections\
+    // insérer la collecte dans collections
+    const insertCollection = await pool.query(
+      "INSERT INTO collections\
 	(volunteer_id, collection_date, location)\
 	VALUES ($1,$2,$3) RETURNING id",
-	[volunteer_id, collections_date, collections_location]
-	);
-  const collection_id = insertCollection.rows[0].id;
-  console.log(collection_id);
-// insérer des infos dans la table quantities 
-//!unnest() permet de reansformer le tableau en lignes verticales dans la BDD
-  const insertQuantities = await pool.query(
-    "INSERT INTO quantities (collection_id, category_id, quantity)\
-    SELECT $1, id, unnest($2::int[])\
-    FROM categories\
-    ORDER BY categories.id\
-    ",
-    [collection_id,quantities]
-  );
-//*renvoyer un message alerte
-	return res.status(201).json({
-		ok: true,
-		message: `collecte ajoutée par ${volunteers_name}`,
-	});
-} catch (error) {
-console.error("erreur lors de la création de la collecte", error);
-}
+      [volunteer_id, collections_date, collections_location]
+    );
+    const collection_id = insertCollection.rows[0].id;
+    console.log(collection_id);
+    //récupérer tous les id de la tablea categories
+    const getIds = await pool.query("SELECT id FROM categories ORDER BY id");
+    const categoryIds = getIds.rows;
+    console.log(categoryIds);
+    if (categoryIds.length !== quantities.length) {
+      return res
+        .status(400)
+        .json({
+          OK: false,
+          message:
+            "Le nombre de quantités ne correspond pas au nombre de catégpries exsistantes",
+        });
+    }
+    //insérer des infos dans la table quantities
+    for (let i = 0; i < categoryIds.length; i++) {
+      const id = categoryIds[i].id;
+      const qty = quantities[i];
+      const insertQuantities = await pool.query(
+        "INSERT INTO quantities (collection_id, category_id, quantity)\
+        VALUES ($1, $2, $3)",[collection_id, id, qty]
+      );
+    };
+
+    //*renvoyer un message alerte
+    return res.status(201).json({
+      ok: true,
+      message: `collecte ajoutée par ${volunteers_name}`,
+    });
+  } catch (error) {
+    console.error("erreur lors de la création de la collecte", error);
+  }
 });
 //🚀route pour récupérer toutes les catégories déchets
-app.get("/categories", async (req, res) => {  
+app.get("/categories", async (req, res) => {
   try {
-  const result = await pool.query('SELECT name from categories ORDER BY id')
-  res.json(result.rows)
+    const result = await pool.query("SELECT name from categories ORDER BY id");
+    res.json(result.rows);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 });
 
@@ -133,4 +152,6 @@ app.get("/my_collection/:id/:location/:date", async (req, res) => {
     res.status(500).json({ error: "Erreur serveur" });
   }
 });
-app.listen(3000, () => {  console.log("Serveur lancé sur http://localhost:3000");});
+app.listen(3000, () => {
+  console.log("Serveur lancé sur http://localhost:3000");
+});
